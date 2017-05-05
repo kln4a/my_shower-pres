f084213b6bb1190e0ae3304893440d252d0b5325
Anton <sirotkin90@gmail.com>
2017-05-05 07:31:41 +0300

    допилен доклада

--- demo/shadow_dom/script.js
+++ demo/shadow_dom/script.js
@@ -1,10 +1,21 @@
 var start = function(){
-  var element = document.getElementById('bu');
-  var shadowRoot = element.attachShadow({
-    mode: 'open'
+  var element,shadowRoot;
+  newDOM.addEventListener('click', function(){
+    element = document.getElementById('bu');
+    shadowRoot = element.attachShadow({
+      mode: 'open'
+    });
   });
-  shadowRoot.innerHTML ='<span>Работает</span>';
-  element.shadowRoot.querySelector('span').textContent = 'Не работает';
+  add.addEventListener('click', function(){
+    shadowRoot.innerHTML ='<span>Не работает</span>';
+  });
+  replace.addEventListener('click', function(){
+    element.shadowRoot.querySelector('span').textContent = 'Работает';
+  });
+  content.addEventListener('click', function(){
+    shadowRoot.innerHTML = shadowRoot.innerHTML + '<slot name=slot1></slot>';
+  });
+
 };
 
 document.addEventListener('DOMContentLoaded', start);
